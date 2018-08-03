62ad4cf33a8857a88d07264d0bc0cfb08d63470e
meswaramoorthy <maha2804+coding@gmail.com>
2018-08-03 23:09:59 +0530

    Added BitesOfPy file

---
+++ Python/BitesOfPy/IntroBites/Bite104SplitandJoin.py
@@ -0,0 +1,19 @@
+message = """Hello world!
+We hope that you are learning a lot of Python.
+Have fun with our Bites of Py.
+Keep calm and code in Python!
+Become a PyBites ninja!"""
+
+def split_in_columns(message=message):
+    """Split the message by newline (\n) and join it together on '|'
+       (pipe), return the obtained output"""
+    splitstring = message.split("\n")
+
+    joinstring=''
+    for split in splitstring:
+        joinstring += split+'|'
+
+    print('|'.join(splitstring))
+    return joinstring
+
+print(split_in_columns(message))
