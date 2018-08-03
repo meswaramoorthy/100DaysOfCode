3fcc20cf12ef2f1e63d66cc10ae76ed56fcb5a8f
meswaramoorthy <maha2804+coding@gmail.com>
2018-08-03 12:56:33 +0530

    HackerEarth pbms

---
+++ Python/HackerEarth/Input and Output Problems/JadoovKoba.py
@@ -0,0 +1,16 @@
+#a = int(input('Enter start number'))
+#b = int(input('Enter end number'))
+
+"""Jadoo, the space alien has challenged Koba to a friendly duel.
+ He asks Koba to write a program to print out all numbers from 70 to 80.
+  Knowing perfectly well how easy the problem is, the Jadoo adds his own twist to the challenge.
+   He asks Koba to write the program without using a single number in his program
+    and also restricts the character limit to 100. """
+
+a = ord('F')
+b = ord('P')
+c = a
+while c <= b:
+    print(c)
+    c += ord('B') - ord('A')
+
